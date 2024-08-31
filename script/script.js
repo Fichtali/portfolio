c777a7e599508f38ddf2e35ccb568122ec90905f
Fichtali <92021986+Fichtali@users.noreply.github.com>
2024-08-31 21:48:18 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -25,13 +25,13 @@ function buttonanimate(e) {
     })
 }
 
-function showDetails(e) {
-    const myStansGap = e.querySelector('#stansGap');
-    const myCardDetails = e.querySelector('#cardDetails');
+// function showDetails(e) {
+//     const myStansGap = e.querySelector('#stansGap');
+//     const myCardDetails = e.querySelector('#cardDetails');
 
-    myStansGap.style.height = '0%'
-    myCardDetails.style.height = '100%'
-}
+//     myStansGap.style.height = '0%'
+//     myCardDetails.style.height = '100%'
+// }
 function hideDetails(e) {
     const myStansGap = e.querySelector('#stansGap');
     const myCardDetails = e.querySelector('#cardDetails');
